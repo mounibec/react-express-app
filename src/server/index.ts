9f079c0561f250ef9d3cc2e9ff41c9c53f18bc9b
mounibec <mounibec@gmail.com>
2019-02-20 18:49:14 +0100

    Refactoring web server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,43 +1,4 @@
-import * as express from 'express';
-import * as http from 'http';
-
-import Conf from './configuration';
-import db from './models';
-import Router from './router';
-import Middleware from './middleware';
-
-export class WebServer {
-  _httpServer: http.Server = null;
-
-  async connectToDB() {
-    return db.sequelize.authenticate();
-  }
-
-  async start() {
-    const configuration = Conf.get();
-
-    await this.connectToDB();
-
-    const app = express();
-    this._httpServer = new http.Server(app);
-
-    app.set('case sensitive routing', true);
-    app.set('strict routing', true);
-    app.set('x-powered-by', false);
-    app.set('view engine', 'html');
-
-    const router = new Router();
-    const middleware = new Middleware(configuration.path);
-
-    middleware.before(app);
-    router.configure(app);
-    middleware.after(app);
-
-    this._httpServer.listen(configuration.port, () => {
-      console.log(`Server listening on ${configuration.port}`);
-    });
-  }
-}
+import WebServer from './web-server';
 
 process.on('uncaughtException', (err) => {
   console.error('uncaughtException', err);
@@ -48,4 +9,12 @@ process.on("unhandledRejection", (reason, promise) => {
   throw reason;
 });
 
-new WebServer().start();
+void (async () => {
+  try {
+    await new WebServer().start();
+    console.log(`${new Date}: Server Started`);
+  } catch (e) {
+    console.error('RUN_ERROR', e.stack);
+    process.exit(1);
+  }
+})();
